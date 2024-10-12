1e5faaf3015d3f2bc1b3fbe2a1daa9d05cb9d41a
kabita-islam <kabitaislam30@gmail.com>
2024-10-12 22:55:16 +0600

    addedd

---
+++ function.php
@@ -0,0 +1,40 @@
+<?php
+
+// function add($a,$b){
+
+//     echo $a+$b;
+// }
+
+// add(2,3);
+
+
+
+
+
+// function star(){
+//     for($i= 0; $i< 5; $i++){
+//         for($j= 0; $j< $i; $j++){
+//         echo"*";
+//         }
+//         echo "\n";
+//     }
+// }
+// star(10) ;
+
+
+// function name($fName,$lName){
+//    // echo "hi"." ".$fName." ".$lName."\n";
+//    echo "hi $fName $lName\n" ;
+// }
+// name("kabita","islam") ;
+
+
+
+/////////////////////  funtion with default value ////////////
+
+function name($fName,$lName="islam"){
+    // echo "hi"." ".$fName." ".$lName."\n";
+    echo "hi $fName $lName\n" ;
+ }
+ name("kabita") ;
+?> 
